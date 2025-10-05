6846c97061bca2ac00c7581df7f70f8622b33d23
Ngô Thị Thúy Nhi <momonhi2005@gmail.com>
2025-10-05 08:44:39 +0700

    Test lại code

--- fe/src/components/Customer/ChiTietTour.vue
+++ fe/src/components/Customer/ChiTietTour.vue
@@ -55,7 +55,7 @@
                         </button>
                     </div>
                     <div class="position-absolute top-0 start-0 translate-middle"
-                        style="background:#d32f2f; color:white; font-weight:bold; padding:6px 15px; font-size:14px; border-radius:2px; margin-left: 132px; margin-top: 50px;">
+                        style="background:#d32f2f; color:white; font-weight:bold; padding:6px 15px; font-size:14px; border-radius:2px; margin-left: 140px; margin-top: 50px;">
                         Giảm 4 Triệu Nhóm 4 | 34.899k/khách
                         <!-- Tam giác bên trái -->
                         <div style="position:absolute; left:0; bottom:-8px;
@@ -64,62 +64,196 @@
                 border-top:8px solid #a62828;"></div>
                     </div>
                 </div>
+
+                <!-- Thanh tab -->
+                <nav id="navbar-example2" class="navbar bg-body-tertiary px-3 mb-3"
+                    style="position:fixed;top:0;left:0px;width:100%;z-index:1000; background-color: antiquewhite;">
+                    <ul class="nav nav-pills justify-content-center" style="padding-left: 90px;">
+                        <li class="nav-item">
+                            <a class="nav-link text-black" style="font-size: 17px;" href="#scrollspyHeading1">Lịch khởi
+                                hành</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-black" style="font-size: 17px;" href="#scrollspyHeading2">Tổng
+                                quan</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-black" style="font-size: 17px;" href="#scrollspyHeading3">Lịch
+                                trình</a>
+                        </li>
+
+
+                        <li class="nav-item">
+                            <a class="nav-link text-black" style="font-size: 17px;" href="#scrollspyHeading4">Chương
+                                trình
+                                tour</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link text-black" style="font-size: 17px;" href="#scrollspyHeading5">Thông tin
+                                cần lưu
+                                ý</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-black" style="font-size: 17px;" href="#scrollspyHeading6">Đánh giá
+                                của khách
+                                hàng</a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-root-margin="0px 0px -40%"
+                    data-bs-smooth-scroll="true" class="scrollspy-example bg-body-tertiary p-3 rounded-2" tabindex="0">
+                    <h4 id="scrollspyHeading1">Lịch khởi hành & giá tour</h4>
+                    <p>
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex justify-content-end text-danger">
+                                <b style="font-size: 20px;">10/10/2025</b>
+                            </div>
+                            <!-- Phương tiện di chuyển -->
+                            <div class="d-flex justify-content-center">
+                                <b style="font-size: 20px; color: #0066CC;">Phương tiện di chuyển</b>
+                            </div>
+                            <div class="row mt-2 mb-2">
+                                <div class="d-flex">
+                                    <div class="pe-3">
+                                        <div class="d-flex justify-content-between">
+                                            <span>Ngày đi - 10/10/2025</span>
+                                            <div style="margin-left: 130px;" class="text-primary">
+                                                <i class="fa-solid fa-plane-departure"></i>
+                                                Máy bay
+                                            </div>
+                                        </div>
+                                        <div style="display: flex; flex-direction: column; align-items: center;">
+                                            <!-- Mốc thời gian -->
+                                            <div
+                                                style="display: flex; justify-content: space-between; width: 100%; font-weight: bold;">
+                                                <span>11:35</span>
+                                                <span>20:40</span>
+                                            </div>
+
+                                            <!-- Thanh ngang -->
+                                            <div style="position: relative; width: 90%; height: 10px; margin-top: 5px;">
+                                                <div
+                                                    style="height: 2px; background: #bbb; position: absolute; top: 4px; left: 0; right: 0;">
+                                                </div>
+                                                <div
+                                                    style="width: 9px; height: 9px; background: #aaa; position: absolute; left: 0; top: 0; border-radius: 2px;">
+                                                </div>
+                                                <div
+                                                    style="width: 9px; height: 9px; background: #aaa; position: absolute; right: 0; top: 0; border-radius: 2px;">
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                    <div class="ps-3 border-start">
+                                        <div class="d-flex justify-content-between">
+                                            <span>Ngày về - 10/10/2025</span>
+                                            <div style="margin-left: 120px;" class="text-primary">
+                                                <i class="fa-solid fa-plane-departure"></i>
+                                                Máy bay
+                                            </div>
+                                        </div>
+                                        <div style="display: flex; flex-direction: column; align-items: center;">
+                                            <!-- Mốc thời gian -->
+                                            <div
+                                                style="display: flex; justify-content: space-between; width: 100%; font-weight: bold;">
+                                                <span>11:35</span>
+                                                <span>20:40</span>
+                                            </div>
+
+                                            <!-- Thanh ngang -->
+                                            <div style="position: relative; width: 90%; height: 10px; margin-top: 5px;">
+                                                <div
+                                                    style="height: 2px; background: #bbb; position: absolute; top: 4px; left: 0; right: 0;">
+                                                </div>
+                                                <div
+                                                    style="width: 9px; height: 9px; background: #aaa; position: absolute; left: 0; top: 0; border-radius: 2px;">
+                                                </div>
+                                                <div
+                                                    style="width: 9px; height: 9px; background: #aaa; position: absolute; right: 0; top: 0; border-radius: 2px;">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="d-flex justify-content-center">
+                                <b style="font-size: 20px; color: #0066CC;">Giá</b>
+                            </div>
+                            <div class="row mt-2">
+                                <!-- Cột người lớn -->
+                                <div class="col-6">
+                                    <div class="d-flex justify-content-between">
+                                        <div>
+                                            <b>Người lớn:</b>
+                                            <div style="color: #aaa; font-size: 15px;">(Từ 12 tuổi trở lên)</div>
+                                        </div>
+                                        <div style="color: red; font-size: 22px; font-weight: bold;">99.000.000đ</div>
+                                    </div>
+
+                                </div>
+
+                                <!-- Cột trẻ em -->
+                                <div class="col-6 border-start">
+                                    <div class="d-flex justify-content-between">
+                                        <div>
+                                            <b>Trẻ em:</b>
+                                            <div style="color: #aaa; font-size: 15px;">(Dưới 12 tuổi trở xuống)</div>
+                                        </div>
+                                        <div style="color: red; font-size: 22px; font-weight: bold;">99.000.000đ</div>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    </p>
+                    <h4 id="scrollspyHeading2">Tổng quan</h4>
+                    <p>...</p>
+                    <h4 id="scrollspyHeading3">Lịch trình</h4>
+                    <p>...</p>
+
+                    <h4 id="scrollspyHeading4">Chương trình tour</h4>
+                    <p>...</p>
+                    <h4 id="scrollspyHeading5">Thông tin cần lưu ý</h4>
+                    <p>...</p>
+                    <h4 id="scrollspyHeading6">Đánh giá khách hàng về</h4>
+                    <p>...</p>
+                </div>
             </div>
 
             <div class="col-lg-4">
-                <div class="card" style="background-color: #FFF4E0;">
-                    <div class="card-body">
-                        <div class="row">
-                            <h5 style="color: #0066CC;">Lịch Trình và Giá Tour</h5>
-                            <p>Chọn Lịch Trình và Xem Giá:</p>
+                <div style="position: sticky; top: 100px;">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title"><b>Giá từ:</b></h5>
+                            <h2 class="text-danger"><b>9500000đ</b><span class="text-black" style="font-size: 22px;"> /
+                                    Khách</span></h2>
+                            <i class="fa-solid fa-ticket me-2"></i>Mã tour: <b>NH123</b>
+                            <p class="mt-2"><i class="fa-solid fa-calendar-days me-2"></i>Ngày khởi hành:</p>
+                            <a href="#scrollspyHeading1">
+                                <button class="btn btn-outline-primary me-3">25/9</button>
+                                <button class="btn btn-outline-primary me-3">29/9</button>
+                                <button class="btn btn-outline-primary me-3">01/10</button>
+                                <button class="btn btn-outline-primary me-3">10/10</button>
+                            </a>
                         </div>
-
                     </div>
                 </div>
+
             </div>
+
         </div>
 
 
 
 
-        <!-- Thanh tab -->
-        <nav id="navbar-example2" class="navbar bg-body-tertiary px-3 mb-3"
-            style="position:fixed;top:0;left:0px;width:100%;z-index:1000; background-color: antiquewhite;">
-            <ul class="nav nav-pills justify-content-center" style="padding-left: 90px;">
-                <li class="nav-item">
-                    <a class="nav-link text-black" style="font-size: 17px;" href="#scrollspyHeading1">Tổng quan</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-black" style="font-size: 17px;" href="#scrollspyHeading2">Chương trình
-                        tour</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-black" style="font-size: 17px;" href="#scrollspyHeading3">Lịch khởi hành</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-black" style="font-size: 17px;" href="#scrollspyHeading4">Thông tin cần lưu
-                        ý</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-black" style="font-size: 17px;" href="#scrollspyHeading5">Đánh giá của khách
-                        hàng</a>
-                </li>
-            </ul>
-        </nav>
 
-        <div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-root-margin="0px 0px -40%"
-            data-bs-smooth-scroll="true" class="scrollspy-example bg-body-tertiary p-3 rounded-2" tabindex="0">
-            <h4 id="scrollspyHeading1">Trải nghiệm thú vị trong tour</h4>
-            <p>...</p>
-            <h4 id="scrollspyHeading2">Chương trình tour</h4>
-            <p>...</p>
-            <h4 id="scrollspyHeading3">Lịch khởi hành & giá tour</h4>
-            <p>...</p>
-            <h4 id="scrollspyHeading4">Thông tin cần lưu ý</h4>
-            <p>...</p>
-            <h4 id="scrollspyHeading5">Đánh giá khách hàng về</h4>
-            <p>...</p>
-        </div>
     </div>
 </template>
 <script>
